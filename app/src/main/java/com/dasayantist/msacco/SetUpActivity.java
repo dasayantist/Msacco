e03e5e1864edf6a45c7863ade35fc702dc37cb89
dasayantist <ngetichk7@gmail.com>
2019-07-19 15:27:07 +0300

    App setting

--- app/src/main/java/com/dasayantist/msacco/SetUpActivity.java
+++ app/src/main/java/com/dasayantist/msacco/SetUpActivity.java
@@ -1,13 +1,92 @@
 package com.dasayantist.msacco;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
 
 public class SetUpActivity extends AppCompatActivity {
+
+    String PhoneType, IMEINumber, SubscriberID, SIMSerialNumber, NetworkCountryISO, SIMCountryISO, SoftwareVersion, strphoneType;
+    static final int PERMISSION_READ_STATE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up);
+        start();
     }
+
+    public void start() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            MyTelephonyManager();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    PERMISSION_READ_STATE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_READ_STATE: {
+                if (grantResults.length>=0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    MyTelephonyManager();
+                } else {
+                    Toast.makeText(this, "you don't have required permission to make the Action", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private void MyTelephonyManager() {
+        TelephonyManager manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        int phoneType = manager.getPhoneType();
+        switch (phoneType) {
+            case (TelephonyManager.PHONE_TYPE_CDMA):
+                ;
+                strphoneType = "CDMA";
+                break;
+            case (TelephonyManager.PHONE_TYPE_GSM):
+                ;
+                strphoneType = "GSM";
+                break;
+            case (TelephonyManager.PHONE_TYPE_NONE):
+                ;
+                strphoneType = "NONE";
+                break;
+        }
+        boolean isRoaming = manager.isNetworkRoaming();
+
+        PhoneType = strphoneType;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        IMEINumber = manager.getDeviceId();
+        SubscriberID =manager.getSubscriberId();
+        SIMSerialNumber = manager.getSimSerialNumber();
+        NetworkCountryISO = manager.getNetworkCountryIso();
+        SIMCountryISO = manager.getSimCountryIso();
+        SoftwareVersion = manager.getDeviceSoftwareVersion();
+
+    Toast.makeText(this, PhoneType + IMEINumber + SubscriberID + SIMSerialNumber + NetworkCountryISO + SIMCountryISO + SoftwareVersion + strphoneType
+    , Toast.LENGTH_SHORT).show();
+
+    }
+
 }
